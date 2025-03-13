45f2d96b66a4a21a6b6d613ed61b73d1baa17c64
vaskiax <jogonzalezm@unal.edu.co>
2025-03-13 12:27:53 -0500

    Update plot linestyle and adjust x-axis limits in Canvas class

--- graphics/canvas.py
+++ graphics/canvas.py
@@ -18,12 +18,11 @@ class Canvas:
 
     def plot(self, x, y, label=None, real_data = False):
         if not real_data:
-            self.ax.plot(x, y, label=label, linestyle='-', linewidth=1)
+            self.ax.plot(x, y, label=label, linestyle='--', linewidth=1)
             if label:
                 plt.legend(loc='best')
         else:
             self.ax.plot(x, y, marker='o', markersize=2, c='black')
-        
 
     def fill(self, bounds: tuple[float, float], bounds_names: tuple[str, str], x_range: np.ndarray, colors:tuple[str, str], color_fill='gray', alpha=0.3):
         """
@@ -47,5 +46,5 @@ class Canvas:
         plt.plot(x_range, bounds[0], color=colors[0], label=bounds_names[0])
         plt.plot(x_range, bounds[1], color=colors[1], label=bounds_names[1])
         plt.ylim(-0.03, 1)
-        plt.xlim(min(x_range), 2)        
+        plt.xlim(min(x_range), max(x_range))        
         plt.legend(loc='best')
